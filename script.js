4249ded0fe6193c725e612c69de98537e24d43de
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-02-04 10:54:41 -0300

    feat: create script.js, create pseudocode for the program

---
+++ script.js
@@ -0,0 +1,81 @@
+console.log("Hello, World!");
+
+/*
+Rock Paper Scissor Pseudo-code
+
+---- program flow -----
+playGame
+---- end of program flow -----
+
+playGame
+  resetScores
+  loop 5 times
+    playRound
+    updateScore
+    displayScore
+  showResult
+  playAgain
+
+resetScores
+  humanScore = 0 
+  computerScore = 0 
+
+playRound
+  humanChoice is getHumanChoice
+  computerChoice is getComputerChoice
+
+  return getWinner, passing choices as parameter
+
+getHumanChoice
+  loop forever
+  prompt the user
+    'choose between rock, paper or scissors'
+  if the user input matches one of the valid choices case insensitive
+    return rock paper or scissor accordingly
+    
+
+getComputerChoice
+  random number between 1 and 3 inclusive
+  return rock, paper or scissors accordingly
+
+getWinner, human and computer choice as parameters
+  rock paper scissors game logic
+    if choices are equal
+      return tie
+    else if human = rock, computer scissor
+      return human
+    else if human = paper, computer rock
+      return human
+    else if human = scissors, computer paper
+      return human
+    else
+      return computer
+
+updateScore, winner as a paremeter
+  if winner is human
+    humanScore += 1
+  if winner is computer
+    computerScore += 1
+  return
+
+displayScore, scores as a parameter
+  Human : humanScore - computerScore : Computer
+
+showResult, scores as a parameter
+  if human won
+    show 'congratulation human!'
+  if computer won
+    show 'oh no! better luck next time!'
+  if tie
+    show 'oof! that was close'
+
+playAgain
+  prompt the user
+    'do you want to play again?'
+  if the user input matches [y, yes] case insensitive
+    humanScore = 0
+    computerScore = 0
+    playGame
+  else
+    show 'thanks for playing'
+*/
